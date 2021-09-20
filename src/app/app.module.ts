6f35f0d2b0ea64fde2eaecd5db478be9fe912a06
Aymen Braiek <Aymen Braiek@cynapsys.local>
2021-09-20 14:19:10 +0100

    @Input +@Output closes #2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EventsComponent } from './components/events/events.component';
+import { EventListComponent } from './components/events/event-list/event-list.component';
+import { EventThumbnailComponent } from './components/events/event-thumbnail/event-thumbnail.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EventsComponent,
+  EventListComponent,
+  EventThumbnailComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/events/event-list/event-list.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-event-list',
+  templateUrl: './event-list.component.html',
+  styleUrls: ['./event-list.component.css']
+})
+export class EventListComponent implements OnInit {
+ 
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+  event1={
+    id:1,
+    name:'angular connect',
+    date:'9/26/2036',
+    time:'angular Connect',
+    price:599.99,
+    location:{
+      address:'1057 DT',
+      city:'London',
+      country:'England'
+
+    }
+
+
+  }
+
+  handleEventClicked(data:String){
+
+    console.log('received',data);
+
+  }
+
+}
